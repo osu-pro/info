ef718b08f736646c7ef2549c5b2b60c8909cdf4e
OSU PRO <osu.pro.727.ppy@gmail.com>
2024-10-04 00:48:03 +0300

    Навигацию подулучшил

--- src/Common/Navigation.tsx
+++ src/Common/Navigation.tsx
@@ -4,9 +4,9 @@ import "./Navigation.css"
 export function Navigation() {
     const classNameGenerator = ({isActive }: {isActive : boolean}) => isActive ? "navigation-button navigation-button-active" : "navigation-button"
     return <div className="navigation-outer">
-        <NavLink className={classNameGenerator} to="/"> Home </NavLink>
-        <NavLink className={classNameGenerator} to="/players"> Players </NavLink>
-        <NavLink className={classNameGenerator} to="/epidemy"> Epidemy </NavLink>
-        <NavLink className={classNameGenerator} to="/phrases"> Phrases </NavLink>
+        <NavLink className={classNameGenerator} to="/info/"> Home </NavLink>
+        <NavLink className={classNameGenerator} to="/info/players"> Players </NavLink>
+        <NavLink className={classNameGenerator} to="/info/epidemy"> Epidemy </NavLink>
+        <NavLink className={classNameGenerator} to="/info/phrases"> Phrases </NavLink>
     </div>
 }
