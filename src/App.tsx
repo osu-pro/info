49b2243b41b5c6222ad43cffd5040a4935013893
OSU PRO <osu.pro.727.ppy@gmail.com>
2024-10-08 02:07:08 +0300

    Линтер фих

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,8 @@ import React from 'react';
 import './App.css';
 import {
   BrowserRouter,
-  Routes,
-  Route
 } from "react-router-dom";
-import {Home} from "./Pages/Home";
-import {Epidemy} from "./Pages/Epidemy";
-import {Players} from "./Pages/Players";
-import {Navigation} from "./Common/Navigation";
 import {Line} from "./Common/Miscellaneous";
-import {Phrases} from "./Pages/Phrases";
-import {Skins} from "./Pages/Skins";
 
 
 function App() {
